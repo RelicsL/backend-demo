d1ae4a898836ba593b07aba1f1dbb1ffd029846a
relics <1281336591@qq.com>
2019-04-12 00:45:03 +0800

    后端环境搭建

---
+++ routes/rank2.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var mg = require('./mg');
+
+/* GET home page. */
+
+// localjhost:3000/news?start=1&count=2
+
+router.get('/', function(req, res, next) {
+  
+  // console.log('news',req.query)
+
+  // let start=req.query.start-0;
+  // let count=req.query.count-0;
+
+  //读库
+  //读库
+  mg({
+    dbName:'1809',
+    collectionName:'rank2'
+  },(collection,client)=>{
+    collection.find().toArray((err,data)=>{
+      if(data.length>0){
+        res.send(data)
+        // res.send(show(data))
+      }else{
+        res.send({error:1,msg:'数据为空'})
+      }
+    })
+  })
+  
+
+});
+
+module.exports = router;
